c756753078f2de257ef076b570c6147b04b64d09
teodorojo <joao.alfredoteodoro@gmail.com>
2020-09-05 12:17:41 -0300

    adicionado variação de cor do texto de acordo com a condição

--- CalculadoraDeJuros/app/src/main/java/com/example/calculadoradejuros/MainActivity.kt
+++ CalculadoraDeJuros/app/src/main/java/com/example/calculadoradejuros/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calculadoradejuros
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -17,9 +18,21 @@ class MainActivity : AppCompatActivity() {
         val tempo = et_tempo.text
         val user = et_user.text
 
+        /*
         val result = cap * (1 + tx * tempo)
+         */
 
-        tv_result.text = result
+        if(result < 1500){
+            tv_result.setTextColor(Color.parseColor("#209E89"))
+            tv_result.text =
+        } else if (result > 3000){
+            tv_result.setTextColor(Color.parseColor("#03DAC5"))
+            tv_result.text =
+        } else {
+            tv_result.setTextColor(Color.parseColor("#5EEBD3"))
+            tv_result.text =
+        }
+
 
     }
 }
