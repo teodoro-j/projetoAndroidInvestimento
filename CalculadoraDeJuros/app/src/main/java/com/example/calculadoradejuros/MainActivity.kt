351210404160e60af91b508d8f0177e2bd6cedc8
teodorojo <joao.alfredoteodoro@gmail.com>
2020-09-05 23:27:24 -0300

    terminado lógica dasa condições, falta realizar teste

--- CalculadoraDeJuros/app/src/main/java/com/example/calculadoradejuros/MainActivity.kt
+++ CalculadoraDeJuros/app/src/main/java/com/example/calculadoradejuros/MainActivity.kt
@@ -14,13 +14,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun calcular(componente:View) {
-        val cap = et_capital.text
-        val tx = et_taxa.text
-        val tempo = et_tempo.text
         val user = et_user.text
+        val cap = et_capital.text.toString().toDouble()
+        val tx = et_taxa.text.toString().toDouble()
+        val tempo = et_tempo.text.toString().toInt()
 
-        if(user > 3){
-            Toast.makeText(this, "Não existe nome com menos de 2 Letras", Toast.LENGTH_LONG).show()
+        if(user.length < 3){
+            Toast.makeText(this, "Não existe nome com de 2 ou menos Letras", Toast.LENGTH_LONG).show()
         } else {
             Toast.makeText(this, "Nome Validade!!", Toast.LENGTH_LONG).show()
         }
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Prencha o próximo campo", Toast.LENGTH_SHORT).show()
         }
 
-        val result = cap * (1 + tx * tempo)
+        val result = realizarCalculo(cap, tx, tempo)
 
         if(result < 1500){
             tv_result.setTextColor(Color.parseColor("#209E89"))
@@ -49,8 +49,9 @@ class MainActivity : AppCompatActivity() {
             tv_result.setTextColor(Color.parseColor("#5EEBD3"))
             tv_result.text = "Sr(a) $user, o seu investimneto está começando a ser produtivo!"
         }
-        */
+    }
 
-
+    fun realizarCalculo(cap:Double, tx:Double, tempo:Int):Double{
+        return cap * (1 + tx * tempo)
     }
 }
